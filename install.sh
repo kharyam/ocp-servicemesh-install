2a65bd99579006762c9aa956e94da307b87e5ee2
Khary Mendez <kmendez@redhat.com>
2022-07-19 14:10:27 -0400

    Update grep

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ oc apply -f openshift-distributed-tracing.yaml
 # Dynamically get the latest stable version for each operator and insstall it
 for operator in elasticsearch-operator jaeger-product kiali-ossm servicemeshoperator
 do
-    version=$(oc get packagemanifests $operator -o jsonpath="{range .status.channels[*]}Channel: {.name} currentCSV: {.currentCSV}{'\n'}{end}" | grep "stable currentCSV" | awk '{print $4}')
+    version=$(oc get packagemanifests $operator -o jsonpath="{range .status.channels[*]}Channel: {.name} currentCSV: {.currentCSV}{'\n'}{end}" | grep " stable currentCSV" | awk '{print $4}')
     echo Installing $operator with latest stable version: $version
     sed -e "s/OPERATOR_VERSION/$version/" $operator.yaml > $operator-versioned.yaml
     oc apply -f $operator-versioned.yaml
